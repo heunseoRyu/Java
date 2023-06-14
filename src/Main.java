2f9b66e16d585905d4830e05549e719efe71206d
heunseoRyu <120763372+heunseoRyu@users.noreply.github.com>
2023-06-14 21:16:35 +0900

    I made the PhoneNumberBook

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.sql.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> pNums = new ArrayList<>();
+        while (true){
+
+            System.out.println("고르세요. 0.종료 1.저장 2.저장목록 보기 3.검색 4.삭제 >> ");
+            Scanner sc = new Scanner(System.in);//숫자 입력받기
+            int choice = sc.nextInt();
+            if (choice == 0){
+                break;
+            }
+            else if (choice == 1) { // 만약 저장하라하고 하면
+                System.out.println("이름을 입력하세요>> ");
+                Scanner sc1 = new Scanner(System.in);
+                String Name = sc1.nextLine();
+                System.out.println("전화번호를 입력하세요>> ");
+                Scanner sc2 = new Scanner(System.in);
+                String pNum = sc2.nextLine();
+                names.add(String.valueOf(Name));
+                pNums.add(String.valueOf(pNum));
+            }
+            else if(choice == 2){
+                for(Integer j=0;j<names.size();j++){
+                    System.out.println(names.get(j) + ":" + pNums.get(j));
+                }
+            }
+            else if(choice==3){
+                int flag = 0;
+                System.out.println("이름을 검색하시오 >> ");
+                Scanner sc3 = new Scanner(System.in);
+                String pn = sc3.nextLine();
+                for(String i: names) {
+                    if (i == pn) {
+                        System.out.println(pNums.get(names.indexOf(i)));
+                        flag = 1;
+                    }
+                }
+                if (flag==0){
+                    System.out.println("없는데?");
+                }
+            }
+            else if(choice==4) {
+                System.out.println("삭제할 이름을 입력하시오 >> ");
+                Scanner sc4 = new Scanner(System.in);
+                String pn = sc4.nextLine();
+                try {
+                    names.remove(names.indexOf(pn));
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("없어 야발");
+                }
+            }
+        }
+    }
+}
